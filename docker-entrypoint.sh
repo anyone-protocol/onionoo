49be222bc1938967215dbb3147575f33c43e6c99
Yurii Kovalchuk <yurii.kovalchuk@fortegrp.com>
2024-02-04 20:06:00 +0200

    ATOR-67 - Fix entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,3 +1,3 @@
 cd /srv/onionoo.torproject.org/onionoo
 
-java -Dcollector.tor.hosts${COLLECTOR_HOST} -Dcollector.host.protocol${COLLECTOR_PROTOCOL} -Dupdater.period.minutes${UPDATER_PERIOD} -Dupdater.offset.minutes${UPDATER_OFFSET} -jar onionoo-8.3-1.30.0-dev.${TYPE}
+java -Dcollector.tor.hosts=${COLLECTOR_HOST} -Dcollector.host.protocol=${COLLECTOR_PROTOCOL} -Dupdater.period.minutes=${UPDATER_PERIOD} -Dupdater.offset.minutes=${UPDATER_OFFSET} -jar onionoo-8.3-1.30.0-dev.${TYPE}
