888295fa2ae2d92e600236cb76ec7303874435fd
Yurii Kovalchuk <yurii.kovalchuk@fortegrp.com>
2024-02-05 16:59:50 +0200

    ATOR-67 - Merge executables to one container

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,3 +1,5 @@
 cd /srv/onionoo.torproject.org/onionoo
 
-java -Dcollector.tor.hosts=${COLLECTOR_HOST} -Dcollector.host.protocol=${COLLECTOR_PROTOCOL} -Dupdater.period.minutes=${UPDATER_PERIOD} -Dupdater.offset.minutes=${UPDATER_OFFSET} -jar onionoo-8.3-1.30.0-dev.${TYPE}
+nohup java -jar onionoo-8.3-1.30.0-dev.war &
+
+java -Dcollector.tor.hosts=${COLLECTOR_HOST} -Dcollector.host.protocol=${COLLECTOR_PROTOCOL} -Dupdater.period.minutes=${UPDATER_PERIOD} -Dupdater.offset.minutes=${UPDATER_OFFSET} -jar onionoo-8.3-1.30.0-dev.jar
