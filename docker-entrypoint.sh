338130f4464b8cc91fd635c5e16705b209c07c34
Yurii Kovalchuk <yurii.kovalchuk@fortegrp.com>
2024-02-08 09:08:38 +0200

    ATOR-67 - Add nomad job for stage, decompose docker setup, improve directory structure for mount

--- docker-entrypoint.sh
+++
@@ -1,5 +0,0 @@
-cd /srv/onionoo.torproject.org/onionoo
-
-nohup java -jar onionoo-8.3-1.30.0-dev.war &
-
-java -Dcollector.tor.hosts=${COLLECTOR_HOST} -Dcollector.host.protocol=${COLLECTOR_PROTOCOL} -Dupdater.period.minutes=${UPDATER_PERIOD} -Dupdater.offset.minutes=${UPDATER_OFFSET} -jar onionoo-8.3-1.30.0-dev.jar
